c091c4e102e70f89b685a85c988e01bada61fafa
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-05-23 12:45:56 +0100

    finishing the whole thing...

--- main.py
+++ main.py
@@ -12,33 +12,32 @@ def main():
     # Getting user inputs Key (to make the 5x5 char matrix) and Plain Text (Message that is to be encrypted)
     key = input("Enter key: ").replace(" ", "").upper()
     plainText = input("Plain Text: ").replace(" ", "").upper()
-
+    # converting the plaintext into digraphs
     convertedPlainText = convertPlainTextToDiagraphs(plainText)
+    # encryption of the text and conversion into a cipher text
     cipherText = "".join(encryption(convertedPlainText, key))
     cipherText = cipherText.replace(',', '')
-    print('Encoded text: ', cipherText)
+    print('playFair cipher Encoded text: ', cipherText)
 
-    decryptedText = decryption(cipherText, key)
-    print('Decoded text: ', decryptedText)
+    # passing this text into the second cipher encryption known as the rail fence cipher
+    # using 3 rails by default
+    railfenceCipherText = encryptRailFence(cipherText, 3)
+    print("Rail Fence encrypted text : ", railfenceCipherText)
+
+    # decrypting the railfence cipher first by passing it through the railFence decryption cipher
+    railfenceDecryptedText = decryptRailFence(railfenceCipherText, 3)
+    print("Rail Fence decrypted text : ", railfenceDecryptedText)
+
+    # passing this through the playfair decryption cipher function
+    decryptedText = decryption(railfenceDecryptedText, key)
+    # print('playfair Cipher Decoded text: ', decryptedText)
 
     decryptedText = [x for x in decryptedText if x != ',']
     digraphText = group(decryptedText, 2)
 
+    # returning the whole text in uppercase...
     newText = convertDigraphsToPlainText(list(digraphText))
     print('original text: ', separator(newText))
-
-
-
-
-    print(encryptRailFence("attack at once", 2))
-    print(encryptRailFence("GeeksforGeeks ", 3))
-    print(encryptRailFence("defend the east wall", 3))
-
-    # Now decryption of the
-    # same cipher-text
-    print(decryptRailFence("GsGsekfrek eoe", 3))
-    print(decryptRailFence("atc toctaka ne", 2))
-    print(decryptRailFence(encryptRailFence("defend the east wall", 3), 3))
 
 
 if __name__ == "__main__":
